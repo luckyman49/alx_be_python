2122c73b6df57e73a2a0fa8826e61e5895c7b0ec
ganiyu lukman <lganiyu43@gmail.com>
2025-11-09 19:43:15 +0100

    fix hours_to_seconds.py correctly

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,7 +1,3 @@
-principal = 1000
-rate = 0.05
-time = 3
-
-interest = principal * rate * time
-
-print(f"The simple interest is: {interest}")x
+hours = 2
+seconds = hours * 3600
+print(f"{hours} hour(s) is {seconds} seconds.")
