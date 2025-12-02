afaecfd9dc3a6762d8c3d54b1c13555a83d9ffa0
ganiyu lukman <lganiyu43@gmail.com>
2025-12-02 13:02:41 +0100

    Fix SimpleCalculator.divide to raise ZeroDivisionError; add test_division

--- programming_paradigm/simple_calculator.py
+++ programming_paradigm/simple_calculator.py
@@ -1,10 +1,15 @@
-lass SimpleCalculator:
+class SimpleCalculator:
     def add(self, a, b):
         return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
 
     def divide(self, a, b):
         if b == 0:
             raise ZeroDivisionError("division by zero")
         return a / b
-
 
--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,14 +12,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(10, 4), 6)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 5), -10)
 
     def test_division(self):
-        # normal division
         self.assertEqual(self.calc.divide(10, 2), 5)
-        # division by zero should raise ZeroDivisionError
         with self.assertRaises(ZeroDivisionError):
             self.calc.divide(5, 0)
 
